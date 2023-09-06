2c0b8588e63aee0abc40923fcde1d71766e50d17
xhico <francgf@gmail.com>
2023-09-06 12:53:55 +0100

    Move to SAVED_INFO

--- EZTV-AutoDownloader.py
+++ EZTV-AutoDownloader.py
@@ -11,25 +11,6 @@ from transmission_rpc import Client
 from Misc import get911, sendErrorEmail
 
 
-def getConfig():
-    """
-    This function reads the configuration data from a JSON file and returns it as a dictionary.
-
-    Parameters:
-    None.
-
-    Returns:
-    data (dict): A dictionary containing the configuration data.
-
-    """
-    # Open the configuration file in read mode
-    with open(CONFIG_FILE) as f:
-        # Load the JSON data from the file into a dictionary
-        data = json.load(f)
-    # Return the dictionary containing the configuration data
-    return data
-
-
 def getLastSeasonEpisode(show):
     """
     Get the last watched episode of a given show from a configuration file.
@@ -42,8 +23,8 @@ def getLastSeasonEpisode(show):
              If the show is not found in the configuration file, the function returns (1, 1) as default.
     """
     try:
-        season = CONFIG[show]["season"]  # get the last watched season for the given show
-        episode = CONFIG[show]["episode"]  # get the last watched episode for the given show
+        season = SAVED_INFO[show]["season"]  # get the last watched season for the given show
+        episode = SAVED_INFO[show]["episode"]  # get the last watched episode for the given show
     except KeyError:  # if the show is not found in the configuration file
         season = 1  # set the default season to 1
         episode = 1  # set the default episode to 1
@@ -52,7 +33,7 @@ def getLastSeasonEpisode(show):
 
 def getTorrents():
     """
-    Downloads the latest torrents for each TV show in the TVSHOWS list.
+    Downloads the latest torrents for each TV show in the CONFIG.keys() list.
 
     Returns:
         A dictionary containing information about the new torrents, with each key being the torrent ID.
@@ -61,7 +42,7 @@ def getTorrents():
     newTorrents = {}
 
     # Iterate over every show
-    for show in TVSHOWS:
+    for show in CONFIG.keys():
 
         # Download last episode JSON
         try:
@@ -73,6 +54,7 @@ def getTorrents():
         except Exception as ex:
             # If there is an error, log it and move on to the next show.
             logger.error("Failed to download show")
+            logger.error(ex)
             continue
 
         # Iterate over every torrent
@@ -114,7 +96,6 @@ def main():
     # Iterate over every torrent
     for torrent in torrents.values():
         show = torrent["show"]
-        imdb_id = torrent["imdb_id"]
         title = torrent["title"]
         magnet_url = torrent["magnet_url"]
         season = torrent["season"]
@@ -126,14 +107,14 @@ def main():
         try:
             YAGMAIL.send(EMAIL_RECEIVER, "Torrent Added - " + show, title)
         except:
-            logger.error("Couldn'not send ADDED email")
+            logger.error("Couldn't send ADDED email")
 
         # Add to CONFIG
-        CONFIG[show] = {"imdb_id": imdb_id, "season": season, "episode": episode}
+        CONFIG[show] = {"season": season, "episode": episode}
 
-    # Update CONFIG
-    with open(CONFIG_FILE, 'w') as outfile:
-        json.dump(CONFIG, outfile, indent=2)
+    # Update SAVED_INFO
+    with open(SAVED_INFO_FILE, 'w') as outfile:
+        json.dump(SAVED_INFO, outfile, indent=2)
 
     # Remove complete torrent
     torrents = TRANSMISSION.get_torrents()
@@ -152,10 +133,15 @@ if __name__ == '__main__':
 
     logger.info("----------------------------------------------------")
 
+    # Open the configuration file in read mode
     CONFIG_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.json")
+    SAVED_INFO_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "saved_info.json")
+    with open(CONFIG_FILE) as inFile:
+        CONFIG = json.load(inFile)
+    with open(SAVED_INFO_FILE) as inFile:
+        SAVED_INFO = json.load(inFile)
+
     API_URL = "https://eztv.re/api/get-torrents?imdb_id="
-    CONFIG = getConfig()
-    TVSHOWS = CONFIG.keys()
 
     # Set Transmission
     TRANSMISSION_HOST = get911('TRANSMISSION_HOST')
